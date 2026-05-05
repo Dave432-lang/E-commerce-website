80fd6a07aa0f756accd0b54907528d49ec79578c
Dave432-lang <dagbanjadavid22@gmail.com>
2026-05-05 01:36:27 +0000

    Add Featured Products Grid component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import ProductGrid from './components/ProductGrid'
 
 function App() {
   return (
     <div className="app-container">
       <Navbar />
       <Hero />
+      <ProductGrid />
     </div>
   )
 }
